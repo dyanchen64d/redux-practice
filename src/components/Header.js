8e42ca9a1cac70760147134d68257db80cd6eddf
chen.zhang <zhangchen64d@gmail.com>
2023-02-18 17:22:26 +0900

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
-import React from "react";
-import Cart from "./Cart";
-import "./Header.css";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import Cart from './Cart';
+import { authActions } from '../store/auth-slice';
+import './Header.css';
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header>
       <nav className="header-nav">
@@ -9,13 +17,18 @@ const Header = () => {
           <li>
             <h2
               className="header-h2"
-              style={{ fontFamily: "monospace", fontSize: "30px" }}
+              style={{ fontFamily: 'monospace', fontSize: '30px' }}
             >
               Redux Shopping App
             </h2>
           </li>
           <li>
             <Cart />
+          </li>
+          <li>
+            <button onClick={logoutHandler} className="logout-btn">
+              Logout
+            </button>
           </li>
         </ul>
       </nav>
